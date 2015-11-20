f98d1e166f8aabf19aecc7544df63faa218e5409
Egor Potemkin <e_potemkin@irknet.ru>
2015-11-20 17:33:41 +0800

    c

--- tcp.py
+++ tcp.py
@@ -8,17 +8,11 @@ class TcpConnection():
     Basic implementation of tcp connection using raw_sockets
     """
     # ip header fields
-    
-
-    
-    
     def __init__(self, src_ip=None, src_port=None, dst_ip=None, dst_port=None):
         self.src_ip = src_ip
         self.src_port = src_port
         self.dst_ip = dst_ip
         self.dst_port = dst_port
-        self.ip_saddr = socket.inet_aton(src_ip)
-        self.ip_daddr = socket.inet_aton(dst_ip)
 
         self.tcp_seq = 0
         self.tcp_ack_seq = 0
@@ -50,15 +44,11 @@ class TcpConnection():
     def tcp_flags(self):
         return self.tcp_fin + (self.tcp_syn << 1) + (self.tcp_rst << 2) + (self.tcp_psh <<3) + (self.tcp_ack << 4) + (self.tcp_urg << 5)
 
-    def tcp_header(self, data='', syn=False, ack=False, psh = False):
+    def tcp_header(self, data='', syn=False, ack=False, psh = False, fin = False):
         # tcp header
-        tcp_source = 1234   # source port
-        tcp_dest = 80   # destination port
-        #tcp_seq = 454
-        tcp_ack_seq = 0
         tcp_doff = 5    
         #tcp flags
-        tcp_fin = 0
+        tcp_fin = int(fin)
         tcp_syn = int(syn)
         tcp_rst = 0
         tcp_psh = int(psh)
@@ -69,11 +59,12 @@ class TcpConnection():
         tcp_urg_ptr = 0
 
         tcp_offset_res = (tcp_doff << 4) + 0
+        tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
 
-        tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, self.tcp_seq, self.tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr)
+        tcp_header = pack('!HHLLBBHHH' , self.src_port, self.dst_port, self.tcp_seq, self.tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr)
 
-        source_address = socket.inet_aton( source_ip )
-        dest_address = socket.inet_aton(dest_ip)
+        source_address = socket.inet_aton( self.src_ip )
+        dest_address = socket.inet_aton(self.dst_ip)
         placeholder = 0
         protocol = socket.IPPROTO_TCP
         tcp_length = len(tcp_header) + len(data)
@@ -81,9 +72,9 @@ class TcpConnection():
         psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
         psh = psh + tcp_header + data;
 
-        tcp_check = checksum(psh)
+        tcp_check = self.checksum(psh)
 
-        return pack('!HHLLBBH' , tcp_source, tcp_dest, self.tcp_seq, self.tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
+        return pack('!HHLLBBH' , self.src_port, self.dst_port, self.tcp_seq, self.tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
 
     def connect(self):
         """ install tcp connection """
@@ -92,30 +83,42 @@ class TcpConnection():
         except socket.error , msg:
             print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
             sys.exit()
+        # bind socket
+        #print self.socket.bind((self.src_ip, self.src_port))
         # send syn
         packet = self.ip_header + self.tcp_header(syn=True)
         self.socket.sendto(packet, (self.dst_ip, 0 ))
+        #sys.exit()
         # receive syn ack
-        response, addr = self.socket.recvfrom(65535)
-        rcvd_ack = struct.unpack('!H', response[4:6])
-        print rcvd_ack
-        # send ack = rcvd syn
-        self.tcp_seq += 1
-        self.tcp_ack_seq = rcvd_ack
+        print "lets rcv"
+        response, addr = self.socket.recv(15)
+        print "rcved"
+        self.calcNextSeqNums(response)
         packet = self.ip_header + self.tcp_header(ack=True)
         self.socket.sendto(packet, (self.dst_ip, 0))
 
     def disconnect(self):
         """ close tcp connection """
+        # send fin
+        packet = self.ip_header + self.tcp_header(fin=True)
+        self.socket.sendto(packet, (self.dst_ip, 0))
 
     def send(self, data):
         """ send data over established connection """
-        packet = self.ip_header + self.tcp_header(ack=True,)
+        packet = self.ip_header + self.tcp_header(ack=True, psh=True, data=data) + data
         self.socket.sendto(packet, (self.dst_ip, 0))
-
+        # recieve ack
+        response, addr = self.socket.recvfrom(65535)
+        self.calcNextSeqNums(response)
 
     def recv(self):
         """ recieve data from socket """
+        # receive ack
+        response, addr = self.socket.recvfrom(65535)
+        # TODO send ACK
+        self.calcNextSeqNums(response)
+        packet = self.ip_header + self.tcp_header(ack=True)
+        self.socket.sendto(packet, (self.dst_ip, 0))
 
     def checksum(self, msg):
         s = 0
@@ -126,3 +129,11 @@ class TcpConnection():
         s = s + (s >> 16);
         s = ~s & 0xffff
         return s
+
+    def calcNextSeqNums(self, rcvddata):
+        print rcvddata
+        #rcvd_ack = struct.unpack('!H', response[4:6])
+        #print "rcvd ack = %s " % rcvd_ack
+        #self.tcp_seq = rcvd_ack
+        #self.tcp_ack_seq = rcvd_seq + rcvd_len
+
